d907c0c4928adbb86a0490aa6ab9ccd4bbae0644
sfxoekun <oekun@seamfix.com>
2023-12-01 22:19:23 +0100

    implement logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import logo from '../assets/logo.svg'
 import CardStyle, { HeaderContainer, Logo, NavLink, Navigation, HeaderLogo } from './Styles'
 import { googleLogout } from '@react-oauth/google';
+import { useAuth } from './AuthContext';
 
-googleLogout();
 function Header() {
-  
+ const { logout } = useAuth();
+ const handleLogout = async () => {
+  try {
+    googleLogout();
+
+    logout();
+  } catch (error) {
+    console.error('Logout failed:', error);
+  }
+}; 
     return (
     <HeaderContainer>
   <HeaderLogo src={logo}></HeaderLogo>
     <Navigation>
-    <button style={{background:'none',border:'none'}} onClick={() => googleLogout()}><NavLink href="/signup">Logout</NavLink></button> 
+    <button style={{background:'none',border:'none'}} onClick={() => handleLogout}><NavLink href="/signup">Logout</NavLink></button> 
     </Navigation>
   </HeaderContainer>
   )
